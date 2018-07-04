bca8cecaee0968d199d2e4a162596fc4ad7379ef
Cooper <18910890210@163.com>
2018-07-04 16:59:22 +0800

    添加短信验证码

--- app/controllers/v1/login_controller.rb
+++ app/controllers/v1/login_controller.rb
@@ -1,19 +1,2 @@
 class V1::LoginController < ApplicationController
-  def index
-    render json: {
-      available_platform: ['wechat', 'weibo']
-    }
-  end
-
-  def show
-
-    if params[:platform] == 'wechat'
-      render json: {
-        userid: 1,
-      }
-    elsif params[:platform] == 'weibo'
-    end
-  end
-
-  private
 end
